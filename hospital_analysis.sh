95f3b0f8f03fdc0f6aeed4975b22c86b84463d9a
httpsriaah <g.hirwakayi@alustudent.com>
2026-09-24 01:59:01 -0700

    Add hospital analysis script

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -6,3 +6,9 @@ process_vitals() {
     mkdir -p reports
 
     echo "Timestamp,Device_ID,Value,Sensor" > reports/critical_alerts.txt
+
+     if [ -f active_logs/heart_rate.log ]; then
+        grep "CRITICAL" active_logs/heart_rate.log | \
+        awk -F',' '{print $1 "," $2 "," $3 ",Heart Rate"}' \
+        >> reports/critical_alerts.txt
+    fi
